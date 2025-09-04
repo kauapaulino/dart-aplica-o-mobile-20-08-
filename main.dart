d17dcdc758b0946ae76c5f784d90e9b111ff14be
kauapaulino <126708315+kauapaulino@users.noreply.github.com>
2025-09-04 00:00:00 +0000

    feat: if statement

--- main.dart
+++ main.dart
@@ -1,1 +1,20 @@
-print ('Hello world!');
+
+import 'dart:io';
+void main()
+if(idade > 18)
+{
+    print('fim do programa');
+    return;
+}
+print('pode entrar na balada')
+
+
+{
+   // int idade = 20;
+   // if(idade >= 18)
+  //  {
+  //      print('pode entrar na balada');
+  //  }
+  //  print('vá embora!');
+//}
+
